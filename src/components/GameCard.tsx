e8b0815554ac96d00ee4f28fd4078fa5d92615d8
shakira <shakirahmedsalih35756@gmail.com>
2025-04-26 17:08:30 +0300

    adding platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import GamePlatformIcons from "./GamePlatformIcons";
 
 interface GameCardProps {
   game: Game;
@@ -11,6 +12,7 @@ const GameCard = ({ game }: GameCardProps) => {
       <Image src={game.background_image} />
       <CardBody>
         <Card.Title fontSize="2xl">{game.name}</Card.Title>
+        <GamePlatformIcons platforms={game.parent_platforms}></GamePlatformIcons>
       </CardBody>
     </Card.Root>
   );
---
+++ src/components/GamePlatformIcons.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { MdPhoneIphone } from "react-icons/md";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: { platform: Platform }[];
+}
+
+const GamePlatformIcons = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    web: BsGlobe,
+    ios: MdPhoneIphone,
+    android: FaAndroid,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map(({ platform }) => {
+        console.log("Platform Id", platform.id)
+        if (Object.keys(iconMap).includes(platform.slug)) {
+            return (
+              <Icon
+                key={platform.id || platform.slug}
+                as={iconMap[platform.slug]}
+              />
+            );
+        }
+      })}
+    </HStack>
+  );
+};
+
+export default GamePlatformIcons;
